01ddafedff8000490a723aa8a245170409727eeb
qsliu <qsliu2017@outlook.com>
2023-12-07 17:17:59 +0800

    day1 part2

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,4 @@
-fn day1(input: &str) -> i32 {
+fn part1(input: &str) -> i32 {
     input
         .split_ascii_whitespace()
         .map(|line| {
@@ -15,8 +15,43 @@ fn day1(input: &str) -> i32 {
 }
 
 #[test]
-fn test_day1() {
-    let my_input = include_str!("../input/day1.example");
-    let n = day1(my_input);
+fn test_part1() {
+    let my_input = include_str!("../input/day1_part1.example");
+    let n = part1(my_input);
     assert_eq!(n, 142);
+}
+
+fn part2(input: &str) -> i32 {
+    input
+        .split_ascii_whitespace()
+        .map(|line| {
+            let (first, last) = [
+                ("one", "one1one"),
+                ("two", "two2two"),
+                ("three", "three3three"),
+                ("four", "four4four"),
+                ("five", "five5five"),
+                ("six", "six6six"),
+                ("seven", "seven7seven"),
+                ("eight", "eight8eight"),
+                ("nine", "nine9nine"),
+            ]
+            .into_iter()
+            .fold(line.to_string(), |s, (from, to)| s.replace(from, to))
+            .as_bytes()
+            .into_iter()
+            .fold((None, None), |(first, last), b| {
+                let n = b.is_ascii_digit().then(|| (b - b'0') as i32);
+                (first.or(n), n.or(last))
+            });
+            dbg!(first.unwrap() * 10 + last.unwrap())
+        })
+        .sum()
+}
+
+#[test]
+fn test_part2() {
+    let input = include_str!("../input/day1_part2.example");
+    let n = part2(input);
+    assert_eq!(n, 281);
 }
