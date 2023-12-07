0df87883d257c8da3100f3b98c489a9dfbc14bd0
qsliu <qsliu2017@outlook.com>
2023-12-07 17:52:46 +0800

    day2part2

--- src/day2.rs
+++ src/day2.rs
@@ -33,3 +33,37 @@ fn test_part1() {
     let n = part1(input);
     assert_eq!(n, 8);
 }
+
+fn part2(input: &str) -> i32 {
+    input
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let (r, g, b) =
+                line.split_once(":")
+                    .unwrap()
+                    .1
+                    .split(";")
+                    .fold((0, 0, 0), |(r, g, b), set| {
+                        let (rx, gx, bx) = set.split(",").fold((0, 0, 0), |(r, g, b), color| {
+                            let (n, color) = color.trim().split_once(" ").unwrap();
+                            let n = n.parse::<i32>().unwrap();
+                            match color {
+                                "red" => (n, g, b),
+                                "green" => (r, n, b),
+                                "blue" => (r, g, n),
+                                _ => (r, g, b),
+                            }
+                        });
+                        (r.max(rx), g.max(gx), b.max(bx))
+                    });
+            r * g * b
+        })
+        .sum()
+}
+#[test]
+fn test_part2() {
+    let input = include_str!("../input/day2_part1.example");
+    let n = part2(input);
+    assert_eq!(n, 2286);
+}
